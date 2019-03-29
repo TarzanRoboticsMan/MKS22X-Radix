3c345d0b0a0d7c2bba15ba8e5016f6d616df9d9f
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-03-29 09:48:56 -0400

    wrote toString

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,18 +1,43 @@
 public class MyLinkedList<E>{
   public class Node{
-  //Three fields
-  private Node next; private Integer data;
-  //Constructor??
-  public Node(Integer value){
-    data = value;
+    //Three fields
+    private Node next; private Integer data;
+    //Constructor??
+    public Node(Integer value){
+      data = value;
+    }
+    public Node(Integer value, Node newNext){
+      data = value;
+      next = newNext;
+    }
+    //Methods
+    public Node next(){return next;}
+    public void setNext(Node other){next = other;}
+    public String toString(){return data;}
   }
-  public Node(Integer value, Node newNext){
-    data = value;
-    next = newNext;
+  //Fields
+  private Node first, last;
+  private int size;
+
+  public String toString(){
+    String ans =first;
+    Node current = first.next();
+    while (current!=null){
+      ans+=", "+current
+      current=current.next();
+    }
+    return ans;
   }
-  //Methods
-  public Node next(){return next;}
-  public void setNext(Node other){next = other;}
-  public String toString(){return data;}
-  }
+  public MyLinkedList()
+  construct an empty list
+  public void clear()
+  reset the list to an empty state. Very similar to the constructor.
+  public boolean/void add(E)
+  add an element to the end of the list (the boolean would be true all the time if you want to conform to list standards)
+  public void extend(MyLinkedList<E> other)
+  in O(1) time, connect the other list to the end of this list.
+  The other list is then reset to size 0 (do not wipe out the nodes, just disconnect them.)
+  This is how you will merge lists together for your radix sort.
+  public E removeFront()
+  remove the 1st element of the list, and return that value.
 }
