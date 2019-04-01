3a1a0015e96747a15505128736c041687b70a6e5
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-04-01 10:40:22 -0400

    should be all done now

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,7 +1,7 @@
 public class MyLinkedList<E>{
   public class Node{
     //Three fields
-    private Node next; private E data;
+    private Node next; public E data;
     //Constructor??
     public Node(E value){
       data = value;
@@ -13,40 +13,44 @@ public class MyLinkedList<E>{
     //Methods
     public Node next(){return next;}
     public void setNext(Node other){next = other;}
-    public String toString(){return data;}
+    public String toString(){return ""+data;}
   }
   //Fields
   public Node first, last;
   public int size;
 
   //Constructor
-  public MyLinkedList();
+  public MyLinkedList(){};
 
   public String toString(){
-    String ans = first;
+    String ans = ""+first;
     Node current = first.next();
     while (current!=null){
-      ans+=", "+current
+      ans+=", "+current;
       current=current.next();
     }
     return ans;
   }
   public void clear(){
-    first,last = null;
+    first=null;last = null;
     size=0;
   }
   public boolean add(E data){
-    last.setNext(new Node(data))
+    last.setNext(new Node(data));
     size++;
     last=last.next();
     return true;
   }
   public void extend(MyLinkedList<E> other){
-    last.next()=other.first;
+    last.setNext(other.first);
     size+=other.size;
     last=other.last;
     other.first=null;other.last=null;other.size=0;
   }
-  public E removeFront()
-  remove the 1st element of the list, and return that value.
+  public E removeFront(){
+    Node ans = first;
+    first=first.next();
+    size--;
+    return ans.data;
+  }
 }
